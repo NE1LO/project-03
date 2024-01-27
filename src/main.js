037c060f5239b3ce18ff03180c7c152e2a286f79
Anton Smeiko <asmeiko13@gmail.com>
2024-01-27 01:10:31 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { getMarkupFilters } from '../js/getMarkup/getMarkupFilters';
 import { renderPageOne } from '../js/render/renderPageOne';
 
 renderPageOne(getMarkupFilters());
-import '../js/pagination';
+// import '../js/pagination';
 // import './js/mobile.menu';
