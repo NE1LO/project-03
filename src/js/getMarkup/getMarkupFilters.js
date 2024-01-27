f0695890a3e08b7e985df1cfcc51348764fd1e76
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2024-01-27 19:18:03 +0200

    finish pagination adn filter

--- src/js/getMarkup/getMarkupFilters.js
+++ src/js/getMarkup/getMarkupFilters.js
@@ -1,8 +1,12 @@
+import { btnRender } from '../btnRender';
 import { allApi } from '../requests';
+
 export const getMarkupFilters = async param => {
   const filterParam = { ...param };
   const DATA = await allApi.getFilters(filterParam);
   const arrayData = DATA.data.results;
+
+  btnRender(DATA.data.totalPages, param.page);
   const markup = arrayData.reduce((html, item) => {
     return (
       html +
