2083d68f74b58a0180ba3761b587b2ea8dd403e5
talamus1972 <talamus1972@gmail.com>
2024-01-28 23:53:11 +0200

    6
    снова переделал модалку

--- src/js/modal-rating.js
+++ src/js/modal-rating.js
@@ -27,6 +27,8 @@ function handleEscKeyPress(event) {
   }
 }
 
+
+
 closeButton.addEventListener('click', closeModal);
 backdrop.addEventListener('click', handleClickOutsideModal);
 document.addEventListener('keydown', handleEscKeyPress);
