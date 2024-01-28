6c3623d670d94b84254c6ac9e84adf80b1eb8065
Serhii Plis <serzh2577@gmail.com>
2024-01-29 02:10:26 +0300

    fix conflict main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-quote;
-import './js/quote';
 import { getMarkupFilters } from './js/getMarkup/getMarkupFilters';
 import { renderPageOne } from './js/render/renderPageOne';
 
 renderPageOne(getMarkupFilters({ page: 1 }));
 import './js/pagination';
 // import './js/mobile.menu';
-
-main;
