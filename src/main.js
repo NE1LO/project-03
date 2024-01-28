bab39952d61171886d64afbfa02994dddbe3d546
Serhii Plis <serzh2577@gmail.com>
2024-01-29 02:12:27 +0300

    fixed conflict main.js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import { renderPageOne } from './js/render/renderPageOne';
 renderPageOne(getMarkupFilters({ page: 1 }));
 import './js/pagination';
 // import './js/mobile.menu';
+// import './js/quote';
